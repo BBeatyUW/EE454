b92554d15112a6a1f378cc23d161894541900438
BBeatyUW <58005386+BBeatyUW@users.noreply.github.com>
2019-11-30 20:01:12 -0800

    Import Data Update
    Changed data import to be function

--- Power System Analysis.py
+++ Power System Analysis.py
@@ -6,6 +6,16 @@ import csv
 import pandas as pd
 import xlrd
 
-df = pd.read_excel(r'system_SampleInput.xlsx',sheet_name='BusData')
+BusData_Location = ['.\Sample System\system_SampleInput.xlsx', 'BusData']
+LineData_Location = ['.\Sample System\system_SampleInput.xlsx', 'LineData']
 
-print(df)
+def import_BusAndLineData(BusData_Location, LineData_Location):
+    BusData = pd.read_excel(BusData_Location[0], sheet_name=BusData_Location[1])
+    LineData = pd.read_excel(LineData_Location[0], sheet_name=LineData_Location[1])
+    return BusData, LineData
+
+
+df_BusData, df_LineData = import_BusAndLineData(BusData_Location, LineData_Location)
+
+print(df_BusData)
+print(df_LineData)
