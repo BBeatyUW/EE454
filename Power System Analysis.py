2d49d59b404d7a7b8e51d5c82108d869d9638ba7
BBeatyUW <58005386+BBeatyUW@users.noreply.github.com>
2019-12-01 06:26:56 -0800

    NotWorking
    Things are not converging

--- Power System Analysis.py
+++ Power System Analysis.py
@@ -1,10 +1,6 @@
 import numpy as np
-#import scipy as sc
-#import math
-#import openpyxl
-#import csv
+from numpy.linalg import inv
 import pandas as pd
-#import xlrd
 
 """
 Supporting Fucntions
@@ -58,16 +54,125 @@ def build_AdmittanceMatrix(LineData, sys_Size):
     return sys_G, sys_B
 
 """
-
+Parses intial bus information from data
+Takes in Bus Data data frame
+Returns sys_:
+    LoadP - active power consumed at node
+    LoadQ - reactive power consumed at node
+    BusType - type of bus<(S)lack, (G)enerator, (D)rain>
+    PGen - Active Power produced by each generator node
+    VRef - Reference voltages at PV busses
 """
 def init_BusData(BusData):
     col = np.array(BusData.columns)
     sys_LoadP = np.array(BusData[col[1]])
     sys_LoadQ = np.array(BusData[col[2]])
-    sys_Type = np.array(BusData[col[3]])
-    sys_P = np.array(BusData[col[4]])
-    sys_V = np.array(BusData[col[5]])
-    return sys_BusData
+    sys_BusType = np.array(BusData[col[3]])
+    sys_PGen = np.array(BusData[col[4]])
+    sys_VRef = np.array(BusData[col[5]])
+    return sys_LoadP, sys_LoadQ, sys_BusType, sys_PGen, sys_VRef
+
+"""
+Initializes System Data for processing
+Takes in sys_:
+    LoadP - active power consumed at node
+    LoadQ - reactive power consumed at node
+    BusType - type of bus<(S)lack, (G)enerator, (D)rain>
+    PGen - Active Power produced by each generator node
+    VRef - Reference voltages at PV busses
+Returns a 2D array containing each node's current information
+[0] - Voltage (V)
+[1] - Angle (T)
+[2] - Active Power (P)
+[3] - Reactive Power (Q)
+"""
+def init_SysData(sys_LoadP, sys_LoadQ, sys_BusType, sys_PGen, sys_VRef):
+    n= sys_LoadP.size
+    sys_Data = np.zeros((n,4))
+    sys_Data[:,0] = sys_VRef.copy()
+    sys_Data[:,1] = np.zeros(n)
+    """
+    for i in range(n):
+        for j in range(n):
+            if sys_BusType[i]=='G':
+                sys_Data[i,2]=sys_PGen[i]
+            else:
+                sys_Data[i, 2] += sys_Data[i, 0]*sys_Data[j,0]*(sys_G[i,j]*np.cos(sys_Data[i,1]-sys_Data[j,1])+sys_B[i,j]*np.sin(sys_Data[i,1]-sys_Data[j,1]))
+            sys_Data[i, 3] += sys_Data[i, 0]*sys_Data[j,0]*(sys_G[i,j]*np.sin(sys_Data[i,1]-sys_Data[j,1])-sys_B[i,j]*np.cos(sys_Data[i,1]-sys_Data[j,1]))
+    """
+    sys_Data[:,2] = -1*sys_LoadP[sys_BusType=='D']
+    sys_Data[:,3] = -1*sys_LoadQ[sys_BusType=='D'] 
+           
+    return sys_Data
+
+"""
+Determines Jacobian value for a given cell
+Takes in: i, j, n, V_i, V_j, T_i, T_j, P_i, Q_i, G_ij, B_ij
+Returns Jacobian cell value
+"""
+def Jacobian_PowerFlow(i, j, n, V_i, V_j, T_i, T_j, P_i, Q_i, G_ij, B_ij):
+    if (i<n and j<n ): #J_11 dP/dT
+        if(i==j):
+            J = -Q_i-B_ij*V_i**2
+        else:
+            J = V_i*V_j*(G_ij*np.sin(T_i-T_j)-B_ij*np.cos(T_i-T_j))
+    elif(i<n and j>=n ): #J_12 dP/dV
+        if(i==j):
+            J = (P_i/V_i) + G_ij*V_i
+        else:
+            J = V_i*(G_ij*np.cos(T_i-T_j)+B_ij*np.sin(T_i-T_j))
+    elif(i>=n and j<n ): #J_21 dQ/dT
+        if(i==j):
+            J = P_i-G_ij*V_i**2
+        else:
+            J = -V_i*V_j*(G_ij*np.cos(T_i-T_j)+B_ij*np.sin(T_i-T_j))
+    else: #(i>=n and j>= n) J_22 dQ/dV
+        if(i==j):
+            J = (Q_i/V_i)-B_ij*V_i
+        else:
+            J = V_i*(G_ij*np.sin(T_i-T_j)-B_ij*np.cos(T_i-T_j))
+    return J
+
+"""
+Processes 1 iteration of current system data
+Takes in sys_Data, a 2D array containing each node's current information
+[0] - Voltage (V)
+[1] - Angle (T)
+[2] - Active Power (P)
+[3] - Reactive Power (Q),
+As well as, the systems G and B matrices, and node types
+Returns the updated array
+"""
+def update_SysData(sys_Data, sys_G, sys_B, sys_BusType):
+    n=sys_Data.shape[0]
+    """ Determine Jacobian """
+    J = np.zeros((2*(n-1),2*(n-1)))
+    for i in range(2*(n-1)):
+        for j in range(2*(n-1)):
+            J[i,j] = Jacobian_PowerFlow(i, j, (n-1), sys_Data[(i+1)%n,0], sys_Data[(j+1)%n,0],\
+                 sys_Data[(i+1)%n,1], sys_Data[(j+1)%n,1], sys_Data[(i+1)%n,2], sys_Data[(i+1)%n,3],\
+                 sys_G[(i+1)%n,(j+1)%n], sys_B[(i+1)%n,(j+1)%n], )
+    
+    """ Determine inverse of Jacobian """
+    J_inv = inv(J)
+    
+    """ Calculate Delta V's, Theta's, and update """
+    
+    Delta = J_inv @ (np.append(sys_Data[1:n,2], sys_Data[1:n,3]))  
+    print(Delta)     
+    for i in range((n-1)):
+        if sys_BusType[i]!='G':
+            sys_Data[i+1, 0] += Delta[i]
+        sys_Data[i+1, 1] += Delta[i+(n-1)]
+        
+    """ Update P,Q """
+    for i in range(n):
+        for j in range(n):
+            if sys_BusType[i]!='G':
+                sys_Data[i, 2] += sys_Data[i, 0]*sys_Data[j,0]*(sys_G[i,j]*np.cos(sys_Data[i,1]-sys_Data[j,1])+sys_B[i,j]*np.sin(sys_Data[i,1]-sys_Data[j,1]))
+            sys_Data[i, 3] += sys_Data[i, 0]*sys_Data[j,0]*(sys_G[i,j]*np.sin(sys_Data[i,1]-sys_Data[j,1])-sys_B[i,j]*np.cos(sys_Data[i,1]-sys_Data[j,1])) 
+    
+    return sys_Data
 
 """
 ########################
@@ -81,25 +186,55 @@ First variable is for file location, Second variable is for sheet name
 """
 BusData_Location = ['.\Sample System\system_SampleInput.xlsx', 'BusData']
 LineData_Location = ['.\Sample System\system_SampleInput.xlsx', 'LineData']
+tolerance = [.1, .1]
 
 """Data Frame creation for initial Bus and Line Data"""
 df_BusData, df_LineData = import_BusAndLineData(BusData_Location, LineData_Location)
-sys_G, sys_B = build_AdmittanceMatrix(df_LineData, df_BusData.shape[0])
+nodes = 1+np.arange(df_BusData.shape[0])
+sys_G, sys_B = build_AdmittanceMatrix(df_LineData, nodes.size)
+sys_LoadP, sys_LoadQ, sys_BusType, sys_PGen, sys_VRef = init_BusData(df_BusData)
+sys_Data = init_SysData(sys_LoadP, sys_LoadQ, sys_BusType, sys_PGen, sys_VRef)
+
+mismatch_P = np.max(sys_LoadP-sys_Data[:,2])
+mismatch_Q = np.max(sys_LoadQ-sys_Data[:,3])
+mismatch = [abs(mismatch_P), abs(mismatch_Q)]
+iteration = 0
+convergence = [['Iteration', 'Max P Mismatch', 'P Bus', 'Max Q Mismatch', 'Q Bus'],\
+               [iteration, mismatch[0], nodes[sys_LoadP==mismatch_P+sys_Data[:,2]],\
+               mismatch[1], nodes[sys_LoadQ==mismatch_Q+sys_Data[:,3]]]]
+
+while(mismatch>tolerance and iteration < 5):
+    print([iteration, mismatch[0], nodes[sys_LoadP==mismatch_P+sys_Data[:,2]],\
+               mismatch[1], nodes[sys_LoadQ==mismatch_Q+sys_Data[:,3]]])
+    update_SysData(sys_Data, sys_G, sys_B, sys_BusType)
+    mismatch_P = np.max(sys_LoadP-sys_Data[:,2])
+    mismatch_Q = np.max(sys_LoadQ-sys_Data[:,3])
+    mismatch = [abs(mismatch_P), abs(mismatch_Q)]
+    iteration+=1
+    convergence.append([iteration, mismatch[0], nodes[sys_LoadP==mismatch_P+sys_Data[:,2]],\
+               mismatch[1], nodes[sys_LoadQ==mismatch_Q+sys_Data[:,3]]])
+
+
 
 """
 print(sys_G)
 print()
 print(sys_B)
 """
-
 """
 Code for testing purposes
 """
+"""
+dict_BusData = {df_BusData.columns[0]:df_BusData[df_BusData.columns[0]]}
+
+print(df_BusData['P MW'][df_BusData['Type']=='G'])
+
 line_From = np.array(df_LineData['From'])
 line_To = np.array(df_LineData['To'])
 busNum = np.array(df_BusData['Bus #'])      
 ind = np.array(df_LineData.columns)
 line_R = np.array(df_LineData[ind[2]])
+"""
 
 """
 ind1 = np.array(line_From==1)
@@ -112,12 +247,22 @@ for i in range(df_BusData.shape[0]):
 #print(df_BusData)
 #print(ind.size)
 #print(build_AdmittanceMatrix(df_LineData))
-
-test = np.array([[i for j in range(2)] for i in range(2)], dtype = complex)
+"""
+"""
+test = np.array([[2*i+j for j in range(2)] for i in range(2)], dtype = complex)
 print(test)
-print(test[:,0])
+print(test[:,0].size)
+test2 = np.zeros((2,3))
+test2[:,1] = np.array([4,5])
+print(test2)
+#"""
 
+"""
 test1 = 0.5*np.ones((2,2))
 test2 = 1/test1
 #print(test2)
+"""
+"""
+testshape = np.array([[1,2,3],[1,2,3]])
+print(testshape.shape)
 """
