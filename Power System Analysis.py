5305a6468654e66729fcf9b38a9ea94bc046a7fb
BBeatyUW <58005386+BBeatyUW@users.noreply.github.com>
2019-11-30 20:03:45 -0800

    Update Power System Analysis.py

--- Power System Analysis.py
+++ Power System Analysis.py
@@ -6,9 +6,14 @@ import csv
 import pandas as pd
 import xlrd
 
+#Loction for Bus and line data
+#First variable is for file location, Second variable is for sheet name
 BusData_Location = ['.\Sample System\system_SampleInput.xlsx', 'BusData']
 LineData_Location = ['.\Sample System\system_SampleInput.xlsx', 'LineData']
 
+#Imports Bus and line data from excel sheets
+#Takes in an array containing ['File Location', 'Sheet Name']
+#Returns two panda data frames for the bus and line data
 def import_BusAndLineData(BusData_Location, LineData_Location):
     BusData = pd.read_excel(BusData_Location[0], sheet_name=BusData_Location[1])
     LineData = pd.read_excel(LineData_Location[0], sheet_name=LineData_Location[1])
