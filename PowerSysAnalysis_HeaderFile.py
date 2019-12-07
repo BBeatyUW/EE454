f66859e677d2a6f9fef5b982cc428148a250ceaf
BBeatyUW <58005386+BBeatyUW@users.noreply.github.com>
2019-12-06 21:41:04 -0800

    Created Power Flow Analysis function

--- PowerSysAnalysis_HeaderFile.py
+++ PowerSysAnalysis_HeaderFile.py
@@ -290,7 +290,6 @@ def LineFlowResults (sys_Data, LineData, sys_Y):
         PowerFlow(V_i, T_i, V_j, T_j, B_tot, y_ij)
         s_ij, p_ij, q_ij = PowerFlow(V_i,T_i,V_j,T_j,B_tot,y_ij)
         
-        print('Line Violation: ', s_ij, LD_val[i:i+1,5])
         if s_ij*100 < (LD_val[i:i+1,5]):
             violation.append('FALSE')
         else:
@@ -383,3 +382,58 @@ def DataOutput(FileName, sys_Data, LineData, sys_Y, iteration_list, mismatch_P_l
         convergencesheet.write(0, col_num, value, header_format)
 
     writer.save()
+
+def PowerFlowAnalysis(BusData_Location, LineData_Location, Output_FileName, tolerance, S_Base):
+    """Data Frame creation for initial Bus and Line Data"""
+    df_BusData, df_LineData = import_BusAndLineData(BusData_Location, LineData_Location)
+    n = df_BusData.shape[0]
+    """Create Admittance Matrix in forms of Y and seperated into G and B"""
+    sys_Y, sys_G, sys_B = build_AdmittanceMatrix(df_LineData, n)
+    """Creation of sys_Data"""
+    sys_BusNum, sys_LoadP, sys_LoadQ, sys_BusType, sys_PGen, sys_VRef = init_BusData(df_BusData)
+    sys_Data = init_SysData(sys_BusNum, sys_LoadP, sys_LoadQ, sys_BusType, sys_PGen, sys_VRef, sys_G, sys_B, S_Base)
+    """Initial Prime for mismatch detetction and storage"""
+    mismatch_P = sys_Data[1:n,4]
+    mismatch_Q = sys_Data[1:n,6]
+    mismatch_max = [max(abs(mismatch_P)), max(abs(mismatch_Q))]
+    iteration = 0
+    iteration_list = []
+    mismatch_P_list = []
+    mismatch_Q_list = []
+    max_P_bus = []
+    max_Q_bus = []
+    iteration_list.append(iteration)        
+    bus_P, = np.where(mismatch_P == max(abs(mismatch_P)))
+    if len(bus_P) == 0:
+        bus_P, = np.where(mismatch_P == -1*max(abs(mismatch_P)))
+    max_P_bus.append(int(bus_P+2))
+    bus_Q, = np.where(mismatch_Q == max(abs(mismatch_Q)))
+    if len(bus_Q) == 0:
+        bus_Q, = np.where(mismatch_Q == -1*max(abs(mismatch_Q)))
+    max_Q_bus.append(int(bus_Q+2))
+    mismatch_P_list.append(max(abs(mismatch_P)))
+    mismatch_Q_list.append(max(abs(mismatch_Q)))
+    
+    """Loop until solution is reached or max iteration is exceeded"""
+    while(iteration<15 and mismatch_max>tolerance):
+        iteration_list.append(iteration)
+        
+        bus_P, = np.where(mismatch_P == max(abs(mismatch_P)))
+        if len(bus_P) == 0:
+            bus_P, = np.where(mismatch_P == -1*max(abs(mismatch_P)))
+        max_P_bus.append(int(bus_P+2))
+        bus_Q, = np.where(mismatch_Q == max(abs(mismatch_Q)))
+        if len(bus_Q) == 0:
+            bus_Q, = np.where(mismatch_Q == -1*max(abs(mismatch_Q)))
+        max_Q_bus.append(int(bus_Q+2))
+        mismatch_P_list.append(max(abs(mismatch_P)))
+        mismatch_Q_list.append(max(abs(mismatch_Q)))
+        
+        sys_Data = update_SysData(sys_Data, sys_G, sys_B, sys_BusType)
+        mismatch_P = sys_Data[1:n,4]
+        mismatch_Q = sys_Data[1:n,6]
+        mismatch_max = [max(abs(mismatch_P)), max(abs(mismatch_Q))]
+        iteration += 1
+    
+    """Export final solution to excel file"""
+    DataOutput(Output_FileName, sys_Data, df_LineData, sys_Y,iteration_list,mismatch_P_list,mismatch_Q_list,max_P_bus,max_Q_bus)
